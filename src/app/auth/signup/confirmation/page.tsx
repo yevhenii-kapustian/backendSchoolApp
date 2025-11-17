b09a195ca2b9b4fce599f25299ea52226a6d0662
Yevhenii <ke1vin.kapustian@gmail.com>
2025-11-17 15:49:11 +0100

    .

--- src/app/auth/signup/confirmation/page.tsx
+++ src/app/auth/signup/confirmation/page.tsx
@@ -4,14 +4,15 @@ const AuthSignupConfirmation = () => {
     return(
         <section>
             <div className="w-full bg-white rounded-lg shadow-xl p-4 sm:p-6 lg:p-8">
-            <h2 className="text-xl sm:text-2xl lg:text-3xl font-bold text-[#02282C] mb-4 sm:mb-6 text-center">Confirmation</h2>
+            <h2 className="text-xl sm:text-2xl lg:text-3xl font-bold text-[#02282C] mb-4 sm:mb-6 text-center">Check Your Email</h2>
 
             <div className="flex flex-col gap-4">
-                <p className="bg-[#01bf0b]">We have sent you an email</p>
-                <p>Please confirm your email to complete registration and <Link className="font-semibold underline" href="/auth/login">log in</Link></p>
-                <button className="mt-4 button-secondary w-full cursor-pointer py-3 text-base font-semibold">
-
-                </button>
+                <p className="text-center">
+                    To verify your identity, you will receive an email shortly to activate your account.
+                </p>
+                <Link href="/auth/login" className="mt-4 button-secondary w-full text-center cursor-pointer py-3 text-base font-semibold">
+                    Log In
+                </Link>
             </div>
         </div>
         </section>
